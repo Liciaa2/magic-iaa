3d587da3d222048d57003720fa2c62270351981f
Liciaa2 <139561000+Liciaa2@users.noreply.github.com>
2025-06-08 22:17:48 -0300

    Delete SFC directory

--- SFC/js/index.js
+++
@@ -1,8 +0,0 @@
-const botaoFiltrar = document.querySelector('.btn-filtrar');
-
-botaoFiltrar.addEventListener('click', function(){
-
-const categoriaSelecionada = document.querySelector('#categoria').value;p
-
-
-});
