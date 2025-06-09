7249acc7712ee22f55405785b1bed3a1f81a096c
Liciaa2 <139561000+Liciaa2@users.noreply.github.com>
2025-06-08 21:05:29 -0300

    Add files via upload

---
+++ SFC/js/index.js
@@ -0,0 +1,8 @@
+const botaoFiltrar = document.querySelector('.btn-filtrar');
+
+botaoFiltrar.addEventListener('click', function(){
+
+const categoriaSelecionada = document.querySelector('#categoria').value;p
+
+
+});
